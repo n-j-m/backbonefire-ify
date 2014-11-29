50eb6dc7c5d71e7bf694cf8022bc27e27fea0c2a
Nick Moore <nick65@gmail.com>
2014-11-29 02:09:49 -0600

    initial

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var insert = require('gulp-insert');
+var replace = require('gulp-replace');
+
+var fs = require('fs');
+
+var paths = {
+  backfire: './backbonefire/src/backbonefire.js',
+  header: './header.txt',
+  footer: './footer.txt',
+  dist: './dist'
+};
+
+gulp.task('build', function() {
+  var headerText = getHeader();
+  var footerText = getFooter();
+
+  return gulp.src(paths.backfire)
+    .pipe(replace('(function(_, Backbone) {', ''))
+    .pipe(replace('})(window._, window.Backbone);', ''))
+    .pipe(insert.wrap(headerText, footerText))
+    .pipe(gulp.dest(paths.dist));
+
+});
+
+function getHeader() {
+  return fs.readFileSync(paths.header);
+}
+
+function getFooter() {
+  return fs.readFileSync(paths.footer);
+}
